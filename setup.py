a27aa43992c0d4b6aa60d255f4813e3de91746c6
esteng <elias.stengel@gmail.com>
2023-04-03 15:00:50 -0400

    update setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ambiguous_parsing',
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    version="1.0",
+    install_requires=[
+    ],
+    description="",
+    author=""
+)
